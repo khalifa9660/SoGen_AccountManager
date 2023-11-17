8dfb6d3691af864c02da9586062bc20af820b9c7
khali <asura.9660@gmail.com>
2023-11-17 14:50:39 +0100

    Mise en place de l’architecture du projet SoGenration

---
+++ Services/EquipeServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace SoGen_AccountManager.Services
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Equipe
+    {
+        private readonly RequestDelegate _next;
+
+        public Equipe(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class EquipeExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Equipe>();
+        }
+    }
+}
+
